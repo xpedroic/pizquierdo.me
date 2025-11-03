a211a411fa12bd3bcc49eb0af52e76f55382233a
xpedroic <pedro.izqcas@gmail.com>
2025-11-03 15:29:59 +0100

    Mejoras de SEO y favicon añadido

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://pizquierdo.netlify.app',
+  integrations: [sitemap()],
 });
+
+
+
+
+  
